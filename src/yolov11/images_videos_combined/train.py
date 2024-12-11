23295cbe0bd1f929615c074252130e949e1713a6
ajaykkumar-nci <157593306+ajaykkumar-nci@users.noreply.github.com>
2024-12-11 17:20:47 +0000

    Delete src/yolov11 directory

--- src/yolov11/images_videos_combined/train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-import os
-
-model = YOLO("yolov8s.pt")
-cfg_path = "/home/ubuntu/Research_Project/yolo_models/cfg"
-
-results = model.train(data=os.path.join(cfg_path,"Visdrone_combined.yaml"), epochs=50, imgsz=640, patience = 5, batch=32)
